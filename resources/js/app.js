41b353b47a06a1aa5188bb3c0e84d70f67d03c29
Dave Samojlenko <dsamojlenko@gmail.com>
2024-02-02 19:31:46 -0500

    cleanup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,3 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
 import { createApp } from "vue";
 import ProgressChart from "./components/ProgressChart.vue";
 
@@ -13,13 +8,3 @@ const app = createApp();
 app.component("progress-chart", ProgressChart);
 
 app.mount("#app");
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
